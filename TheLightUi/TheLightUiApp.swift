3eaf19a97f5fd362ef00478a123b798325b24b64
Vladimir Tretyakov <vladimir.tretjakoff@yandex.ru>
2021-05-03 15:44:19 +0700

    Initial Commit

---
+++ TheLightUi/TheLightUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheLightUiApp.swift
+//  TheLightUi
+//
+//  Created by Владимир on 03.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct TheLightUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
